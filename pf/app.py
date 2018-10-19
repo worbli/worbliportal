29a79b5c09f0772fb783b7ad7711315f0fa88fc3
Charlie Dumont <charlie.e.dumont@gmail.com>
2018-10-19 02:55:35 -0400

    adding sql alchemy, models and bcrypt to core app

--- pf/app.py
+++ pf/app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.bcrypt import Bcrypt
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+import models
 
 
 @app.route('/')
@@ -8,4 +19,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000)
-
