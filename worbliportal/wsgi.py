66d165e234e67bbdee440c738496b03fdffa864c
Charlie Dumont <charlie.e.dumont@gmail.com>
2018-10-27 19:24:47 -0400

    updating to add CORS

--- worbliportal/wsgi.py
+++ worbliportal/wsgi.py
@@ -1,9 +1,10 @@
 """
 wsgi wrapper for uwsgi, ngninx or apache
 """
+from flask_cors import CORS
 from worbliportal import create_app
 
 APP = create_app()
-
+CORS(APP)
 if __name__ == "__main__":
     APP.run("0.0.0.0", port=5000)
