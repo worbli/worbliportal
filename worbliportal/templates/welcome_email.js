724fb0a42f37ccc1e1600c698559ec7dd3d3619a
Will Hill <email.will.in.china@gmail.com>
2018-10-29 09:05:58 +0800

    add: email templates

---
+++ worbliportal/templates/welcome_email.js
@@ -0,0 +1,32 @@
+var htmlEmail = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>[WORBLI] Email Confirmation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
+  <table style="margin: 0; padding: 60px; background-color: #F7F7F7;" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+    <td style="text-align:center">
+      <img src="https://portal-test.worbli.io/esm-bundled/images/email-logo.png" width="200px" style="margin-bottom: 20px;">
+      <table style="margin: 0; padding: 60px; background-color: #FFFFFF; text-align:left; border-radius: 0.4em;" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+        <td><b>Welcome to WORBLI</b></td>
+      </tr>  
+      <tr>
+        <td>
+        </br></br></br>
+          <p>Hello and welcome to the WORBLI Network!</p>
+          <p>We have sent you this message in response to a recent WORBLI account that has been registered under this email. Congratulations on taking the first step to unlocking everything that Blockchain’s Financial District has to offer.</p>
+          <p>If you need assistance or if this activity is not of your own operation, please contact us at support@worbli.io</p>
+        </td>
+        </tr>
+      </table>
+    </td>
+    </tr>
+  </table>
+  </body>
+</html>
+`
