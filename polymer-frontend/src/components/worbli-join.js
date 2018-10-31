2eab0d9318a4f6e71424d78cf89986f363cd9c2f
Amit Rathi <amit.rathi.k@gmail.com>
2018-10-30 21:40:54 -0400

    updated join form to user iron-form, got error validation showing again

--- polymer-frontend/src/components/worbli-join.js
+++ polymer-frontend/src/components/worbli-join.js
@@ -2,6 +2,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/app-route/app-location.js';
 import '@polymer/iron-ajax/iron-ajax.js';
+import '@polymer/iron-form/iron-form.js';
 import '../css/shared-styles.js';
 import { MyURLSetter } from "../mixins/worbli-urlsetter.js";
 
@@ -84,11 +85,15 @@ class WorbliJoin extends MyURLSetter(PolymerElement) {
                     debounce-duration="300">
             </iron-ajax>
             <h2>Join WORBLI</h2>
-            <p>WORBLI is the place to access smarter financial services</p>
-            <input type="text" class="text" placeholder="Email Address" id="email">
-            <label><input type="checkbox" name="checkbox" value="value"> I agree to the <span><a href="/terms/">Terms and Privacy Policy</a></span></label></br>
-            <label><input type="checkbox" name="checkbox" value="value"> I'm happy to recieve marketing communications from WORBLI</label></br></br>
-            <button class="btn-critical" on-click="_sendEmail">Join</button>
+			<p>WORBLI is the place to access smarter financial services</p>
+			<iron-form>
+				<form method="POST">
+					<input type="text" class="text" placeholder="Email Address" id="email" required>
+					<label><input type="checkbox" name="checkbox" value="value" required> I agree to the <span><a href="/terms/">Terms and Privacy Policy</a></span></label></br>
+					<label><input type="checkbox" name="checkbox" value="value" required> I'm happy to recieve marketing communications from WORBLI</label></br></br>
+					<button class="btn-critical" on-click="_sendEmail">Join</button>
+				</form>
+			</iron-form>
             <div class="center">Already on WORBLI? <span on-click="_signIn">Log In</span></div>
     `;
   }
@@ -121,6 +126,7 @@ class WorbliJoin extends MyURLSetter(PolymerElement) {
     }
 
     handleRegister(event, request) {
+		
         var response = request.response;
         console.log("we got ");
         console.log(JSON.stringify(response));
